c18a04d958a77bd0e0c594872e1cc5423d932910
june23rd1987 <june23rd1987@gmail.com>
2020-09-25 14:23:45 +0800

    Update woo_connector.py

--- woocommerce_erpnext/woo_connector.py
+++ woocommerce_erpnext/woo_connector.py
@@ -110,10 +110,10 @@ def batch_sync_items():
         #pprint(r.get("create", []))
         #print("************************")
         for d in r.get("create", []):
-            doc = frappe.get_doc("Item", d)
             print("************************")
             pprint(d)
             print("************************")
+            doc = frappe.get_doc("Item", d)
             ##JUPITER
             if doc.sync_with_woocommerce != 1:
                 print("skipped : %s - %s is sync_with_woocommerce !=1 not allowed to sync" % (doc.item_name, doc.woocommerce_id) )
